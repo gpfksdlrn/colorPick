d7dbdc7d23426859e890d1bd707156e6c0e4bb12
hizdev <gpfksdlrn@naver.com>
2026-04-23 13:02:40 +0900

    chore: 초기 Electron 프로젝트 세팅

---
+++ main.js
@@ -0,0 +1,8 @@
+const { app, BrowserWindow } = require('electron');
+
+app.whenReady().then(() => {
+  const win = new BrowserWindow({ width: 800, height: 600 });
+  win.loadFile('index.html');
+});
+
+app.on('window-all-closed', () => app.quit());
